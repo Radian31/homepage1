8c6f7a443409f248952f9d88bae921c664745619
Adrian Sikorski <radian3113@gmail.com>
2023-02-24 14:55:02 +0100

    added and organizing functions

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-console.log("Hej wszystkim developerom");
-
-let buttonElement = document.querySelector(".js-button");
-
-let changeBackgrund = document.querySelector(".js-changeBackground");
-let body = document.querySelector(".body");
-let innerColor = document.querySelector(".js-innerColor");
-
-changeBackgrund.addEventListener("click", () => {
-    body.classList.toggle("innerBackground");
-    innerColor.innerText = body.classList.contains("innerBackground") ? "jasny" : "ciemny";
-});
+{
+    const welcome = () => {
+        console.log("Hej wszystkim developerom");
+    }
+   
+    
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        const innerColor = document.querySelector(".js-innerColor");
+        body.classList.toggle("innerBackground");
+        innerColor.innerText = body.classList.contains("innerBackground") ? "jasny" : "ciemny";
+    }
+ 
+    
+    const init = () => {
+        let changeBackgrund = document.querySelector(".js-changeBackground");
+    changeBackgrund.addEventListener("click",toggleBackground);
+    welcome();
+    }
+    init ();
+}
