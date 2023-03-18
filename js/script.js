0a6815dc34e8172a268a4f7fd1a8a925f91dec03
Adrian Sikorski <radian3113@gmail.com>
2023-03-18 21:52:21 +0100

    adding small fixes

--- js/script.js
+++ js/script.js
@@ -2,20 +2,21 @@
     const welcome = () => {
         console.log("Hej wszystkim developerom");
     }
-   
-    
+
+
     const toggleBackground = () => {
         const body = document.querySelector(".body");
         const innerColor = document.querySelector(".js-innerColor");
+
         body.classList.toggle("innerBackground");
         innerColor.innerText = body.classList.contains("innerBackground") ? "jasny" : "ciemny";
     }
- 
-    
+
+
     const init = () => {
-        let changeBackgrund = document.querySelector(".js-changeBackground");
-    changeBackgrund.addEventListener("click",toggleBackground);
-    welcome();
+        const changeBackgrund = document.querySelector(".js-changeBackground");
+        changeBackgrund.addEventListener("click", toggleBackground);
+        welcome();
     }
-    init ();
+    init();
 }
